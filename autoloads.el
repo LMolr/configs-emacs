acaa7af9b1ca28e75209e8c52cfc7735be07cc01
Alessandro Molari <molari.alessandro@gmail.com>
2014-12-09 15:10:12 +0000

    init

---
+++ autoloads.el
@@ -0,0 +1,32 @@
+;;; loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(provide 'loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; loaddefs.el ends here
+
+;;;### (autoloads nil nil ("customs.el" "site-start.el") (21636 12019
+;;;;;;  618356 501000))
+
+;;;***
+
+;;;### (autoloads nil "funcs-apropos" "funcs-apropos.el" (21636 11245
+;;;;;;  961724 288000))
+;;; Generated autoloads from funcs-apropos.el
+
+(autoload 'apropos-package "funcs-apropos" "\
+Shows all Lisp symbols are part of a package named PACKAGE-NAME.
+When printing them out, removes the PACKAGE-NAME prefix.
+For example, a search for ``apropos'' will display ``print'', ``command'',
+etc. in the *Apropos* buffer.
+Returns list of symbols and documentation found.
+
+\(fn PACKAGE-NAME)" t nil)
+
+;;;***
---
+++ funcs-apropos.el
@@ -0,0 +1,42 @@
+;;; package --- summary: functions for apropos
+;;;
+;;; author:  Alessandro Molari <molari.alessandro@gmail.com>
+;;;
+;;; license: Apache 2.0
+;;;
+;;; more informations:
+;;; - http://www.emacswiki.org/AproposMode
+;;;
+;;; commentary:
+;;;
+;;; code:
+;;;
+
+
+;;;###autoload
+(defun apropos-package (package-name)
+    "Shows all Lisp symbols are part of a package named PACKAGE-NAME.
+When printing them out, removes the PACKAGE-NAME prefix.
+For example, a search for ``apropos'' will display ``print'', ``command'',
+etc. in the *Apropos* buffer.
+Returns list of symbols and documentation found."
+    (interactive "sEnter the package name: ")
+    (let ((package (concat package-name "-")))
+      (setq apropos-accumulator (apropos (concat "^" package)))
+      (apropos-print nil nil (concat "Package prefix: " package))
+      (set-buffer "*Apropos*")
+      (toggle-read-only)
+      (search-forward "Package prefix: ")
+      (let ((start (point)))
+        (end-of-line)
+        (put-text-property start (point) 'face apropos-symbol-face))
+      (while (not (null (search-forward package nil t)))
+        ;; hide the package prefix
+        (let ((end (point)))
+          (beginning-of-line)
+          (put-text-property (point) end 'invisible t)
+          (end-of-line)))
+          (toggle-read-only)))
+
+
+(provide 'funcs-apropos)
